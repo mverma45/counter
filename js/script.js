4fe1a131cb778c91c25650fd9adcfd893efeed41
Manny Verma <mverma7845@gmail.com>
2021-10-26 10:52:51 -0600

    basic functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-alert("it works")
+// increase number
+let num = document.querySelector('.increase')
+function add() {
+    // let add = num.innerHTML = `${increase++}`
+    alert("adding one");
+}
+
+// reset counter 
+let num2 = document.querySelector('.reset')
+function reset() {
+    // let add = num.innerHTML = `${increase++}`
+    alert("resetting");
+}
+
+// decrease number
+let num3 = document.querySelector('.decrease')
+function decrease() {
+    // let add = num.innerHTML = `${increase++}`
+    alert("subtracting one");
+}
